f12ae857444bb6e606cb4a69a54e09ecdbb5df81
ahmets1 <ahmetsn73@gmail.com>
2018-12-15 01:31:27 +0300

    Add files via upload

---
+++ ahmet.php
@@ -0,0 +1,12 @@
+<?php
+$baglan=mysqli_connect("localhost","root","","yangin__raporu");
+mysqli_query($baglan, "SET NAMES UTF8");
+
+
+
+
+
+
+
+
+?>
---
+++ dashboard.php
@@ -0,0 +1,400 @@
+<?php
+include "ahmet.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		
+		<title>Pike Admin - Free Bootstrap 4 Admin Template</title>
+		<meta name="description" content="Free Bootstrap 4 Admin Theme | Pike Admin">
+		<meta name="author" content="Pike Web Development - https://www.pikephp.com">
+		
+		<!-- Favicon -->
+		<link rel="shortcut icon" href="assets/images/favicon.ico">
+
+		<!-- Bootstrap CSS -->
+		<link href="assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+		
+		<!-- Font Awesome CSS -->
+		<link href="assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+		
+		<!-- Custom CSS -->
+		<link href="assets/css/style.css" rel="stylesheet" type="text/css" />
+		
+		<!-- BEGIN CSS for this page -->
+		<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap4.min.css"/>
+		<!-- END CSS for this page -->
+		
+		
+</head>
+
+<body class="adminbody">
+
+<div id="main">
+
+	<!-- top bar navigation -->
+	<div class="headerbar">
+
+		<!-- LOGO -->
+        <div class="headerbar-left">
+			<a href="index.html" class="logo"><img alt="Logo" src="assets/images/logo.png" /> <span>Admin</span></a>
+        </div>
+
+        
+
+	</div>
+	<!-- End Navigation -->
+	
+ 
+	<!-- Left Sidebar -->
+	<div class="left main-sidebar">
+	
+		<div class="sidebar-inner leftscroll">
+
+			<div id="sidebar-menu">
+        
+			<ul>
+
+					<li class="submenu">
+						<a class="active" href="ahmet.php"><i class="fa fa-fw fa-bars"></i><span> Dashboard </span> </a>
+                    </li>
+
+					<li class="submenu">
+                        <a href="charts.html"><i class="fa fa-fw fa-area-chart"></i><span> Charts </span> </a>
+                    </li>
+					
+					
+                  
+
+					
+					
+          	
+            </ul>
+
+            <div class="clearfix"></div>
+
+			</div>
+        
+			<div class="clearfix"></div>
+
+		</div>
+
+	</div>
+	<!-- End Sidebar -->
+
+
+    <div class="content-page">
+	
+		<!-- Start content -->
+        <div class="content">
+            
+			<div class="container-fluid">
+					
+						<div class="row">
+									<div class="col-xl-12">
+											<div class="breadcrumb-holder">
+													<h1 class="main-title float-left">Dashboard</h1>
+													<ol class="breadcrumb float-right">
+														<li class="breadcrumb-item">Home</li>
+														<li class="breadcrumb-item active">Dashboard</li>
+													</ol>
+													<div class="clearfix"></div>
+											</div>
+									</div>
+						</div>
+						<!-- end row -->
+
+						
+						
+							<div class="row">
+									<div class="col-xs-12 col-md-6 col-lg-6 col-xl-3">
+											<div class="card-box noradius noborder bg-default">
+													<i class="fa fa-file-text-o float-right text-white"></i>
+													<h6 class="text-white text-uppercase m-b-20">Toplam Yangın Sayısı</h6>
+													<h1 class="m-b-20 text-white counter">
+	<?php
+		$bilgi=mysqli_query($baglan,"SELECT COUNT(kayit_no) FROM yangin");
+		if($bilgi)
+	{
+        $tek=mysqli_fetch_array($bilgi);    
+        echo $tek["0"];
+	}
+
+	?>
+													</h1>
+													<span class="text-white"></span>
+											</div>
+									</div>
+
+									<div class="col-xs-12 col-md-6 col-lg-6 col-xl-3">
+											<div class="card-box noradius noborder bg-warning">
+													<i class="fa fa-user-o float-right text-white"></i>
+													<h6 class="text-white text-uppercase m-b-20">Toplam Ana Ekip Sayısı</h6>
+													<h1 class="m-b-20 text-white counter">
+													<?php
+		$bilgi1=mysqli_query($baglan,"SELECT COUNT(ana_ekip_id) FROM ana_ekip_id");
+		if($bilgi1)
+	{
+        $tek1=mysqli_fetch_array($bilgi1);    
+        echo $tek1["0"];
+	}
+
+	?>
+													
+													</h1>
+													<span class="text-white"></span>
+											</div>
+									</div>
+
+									<div class="col-xs-12 col-md-6 col-lg-6 col-xl-3">
+											<div class="card-box noradius noborder bg-info">
+													<i class="fa fa-user-o float-right text-white"></i>
+													<h6 class="text-white text-uppercase m-b-20">Toplam Yard. Ekip Sayısı</h6>
+													<h1 class="m-b-20 text-white counter">
+													
+													<?php
+		$bilgi2=mysqli_query($baglan,"SELECT COUNT(yardimci_ekip_id) FROM yardimci_ekip_id");
+		if($bilgi2)
+	{
+        $tek2=mysqli_fetch_array($bilgi2);    
+        echo $tek2["0"];
+	}
+
+	?>
+													
+													</h1>
+													<span class="text-white"></span>
+											</div>
+									</div>
+
+									<div class="col-xs-12 col-md-6 col-lg-6 col-xl-3">
+											<div class="card-box noradius noborder bg-danger">
+													<i class="fa fa-bell-o float-right text-white"></i>
+													<h6 class="text-white text-uppercase m-b-20">Bildirimler</h6>
+													<h1 class="m-b-20 text-white counter">
+											<?php
+		$bilgi3=mysqli_query($baglan,"SELECT COUNT(sira_no) FROM yangin");
+		if($bilgi3)
+	{
+        $tek3=mysqli_fetch_array($bilgi3);    
+        echo $tek3["0"];
+	}
+
+	?>		
+													
+													
+													</h1>
+													<span class="text-white"></span>
+											</div>
+									</div>
+									
+									
+							</div>
+							<!-- end row -->
+
+
+							
+							<div class="row">
+							
+									<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-6">						
+										<div class="card mb-3">
+											<div class="card-header">
+												<h3><i class="fa fa-line-chart"></i> Items Sold Amount</h3>
+												Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non luctus metus. Vivamus fermentum ultricies orci sit amet sollicitudin.
+											</div>
+												
+											<div class="card-body">
+												<canvas id="lineChart"></canvas>
+											</div>							
+											<div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+										</div><!-- end card-->					
+									</div>
+
+									<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">						
+										<div class="card mb-3">
+											<div class="card-header">
+												<h3><i class="fa fa-bar-chart-o"></i> Colour Analytics</h3>
+												Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non luctus metus. Vivamus fermentum ultricies orci sit amet sollicitudin.
+											</div>
+												
+											<div class="card-body">
+												<canvas id="pieChart"></canvas>
+											</div>
+											<div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+										</div><!-- end card-->					
+									</div>
+									
+									<div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-3">						
+										<div class="card mb-3">
+											<div class="card-header">
+												<h3><i class="fa fa-bar-chart-o"></i> Colour Analytics 2</h3>
+												Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus non luctus metus. Vivamus fermentum ultricies orci sit amet sollicitudin.
+											</div>
+												
+											<div class="card-body">
+												<canvas id="doughnutChart"></canvas>
+											</div>
+											<div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
+										</div><!-- end card-->					
+									</div>
+									
+							</div>
+							<!-- end row -->
+							
+							
+							
+
+
+            </div>
+			<!-- END container-fluid -->
+
+		</div>
+		<!-- END content -->
+
+    </div>
+	<!-- END content-page -->
+    
+	
+
+</div>
+<!-- END main -->
+
+<script src="assets/js/modernizr.min.js"></script>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/moment.min.js"></script>
+		
+<script src="assets/js/popper.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+
+<script src="assets/js/detect.js"></script>
+<script src="assets/js/fastclick.js"></script>
+<script src="assets/js/jquery.blockUI.js"></script>
+<script src="assets/js/jquery.nicescroll.js"></script>
+
+<!-- App js -->
+<script src="assets/js/pikeadmin.js"></script>
+
+<!-- BEGIN Java Script for this page -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+	<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+	<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap4.min.js"></script>
+
+	<!-- Counter-Up-->
+	<script src="assets/plugins/waypoints/lib/jquery.waypoints.min.js"></script>
+	<script src="assets/plugins/counterup/jquery.counterup.min.js"></script>			
+
+	<script>
+		$(document).ready(function() {
+			// data-tables
+			$('#example1').DataTable();
+					
+			// counter-up
+			$('.counter').counterUp({
+				delay: 10,
+				time: 600
+			});
+		} );		
+	</script>
+	
+	<script>
+	var ctx1 = document.getElementById("lineChart").getContext('2d');
+	var lineChart = new Chart(ctx1, {
+		type: 'bar',
+		data: {
+			labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+			datasets: [{
+					label: 'Dataset 1',
+					backgroundColor: '#3EB9DC',
+					data: [10, 14, 6, 7, 13, 9, 13, 16, 11, 8, 12, 9] 
+				}, {
+					label: 'Dataset 2',
+					backgroundColor: '#EBEFF3',
+					data: [12, 14, 6, 7, 13, 6, 13, 16, 10, 8, 11, 12]
+				}]
+				
+		},
+		options: {
+						tooltips: {
+							mode: 'index',
+							intersect: false
+						},
+						responsive: true,
+						scales: {
+							xAxes: [{
+								stacked: true,
+							}],
+							yAxes: [{
+								stacked: true
+							}]
+						}
+					}
+	});
+
+
+	var ctx2 = document.getElementById("pieChart").getContext('2d');
+	var pieChart = new Chart(ctx2, {
+		type: 'pie',
+		data: {
+				datasets: [{
+					data: [12, 19, 3, 5, 2, 3],
+					backgroundColor: [
+						'rgba(255,99,132,1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+						'rgba(75, 192, 192, 1)',
+						'rgba(153, 102, 255, 1)',
+						'rgba(255, 159, 64, 1)'
+					],
+					label: 'Dataset 1'
+				}],
+				labels: [
+					"Red",
+					"Orange",
+					"Yellow",
+					"Green",
+					"Blue"
+				]
+			},
+			options: {
+				responsive: true
+			}
+	 
+	});
+
+
+	var ctx3 = document.getElementById("doughnutChart").getContext('2d');
+	var doughnutChart = new Chart(ctx3, {
+		type: 'doughnut',
+		data: {
+				datasets: [{
+					data: [12, 19, 3, 5, 2, 3],
+					backgroundColor: [
+						'rgba(255,99,132,1)',
+						'rgba(54, 162, 235, 1)',
+						'rgba(255, 206, 86, 1)',
+						'rgba(75, 192, 192, 1)',
+						'rgba(153, 102, 255, 1)',
+						'rgba(255, 159, 64, 1)'
+					],
+					label: 'Dataset 1'
+				}],
+				labels: [
+					"Red",
+					"Orange",
+					"Yellow",
+					"Green",
+					"Blue"
+				]
+			},
+			options: {
+				responsive: true
+			}
+	 
+	});
+	</script>
+<!-- END Java Script for this page -->
+
+</body>
+</html>
